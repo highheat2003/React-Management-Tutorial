394934a3c31ddade9418bf5b4ed092916cb6a5c7
Lee seungchan <highheat@gmail.com>
2019-06-18 11:42:10 +0900

    데이터베이스 테이블 생성 및 연결 설정

---
+++ db.sql
@@ -0,0 +1,6 @@
+INSERT INTO customer VALUES (1, 'https://placeimg.com/64/64/1','이승찬','921212','남자','직장인');
+
+INSERT INTO customer VALUES (2, 'https://placeimg.com/64/64/2','홍길동','921212','남자','직장인');
+
+INSERT INTO customer VALUES (3, 'https://placeimg.com/64/64/3','아무개','921212','남자','직장인');
+
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,34 +7,28 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers',(req, res) => {
 
-    res.send(
-        [{
-            'id': 1,
-            'image': 'https://placeimg.com/64/64/1',
-            'name': '이승찬',
-            'birthday': '921212',
-            'gender': '남자',
-            'job': '직장인'
-          },
-          {
-            'id': 2,
-            'image': 'https://placeimg.com/64/64/2',
-            'name': '홍길동',
-            'birthday': '921212',
-            'gender': '남자',
-            'job': '직장인'
-          },
-          {
-            'id': 3,
-            'image': 'https://placeimg.com/64/64/3',
-            'name': '아무개',
-            'birthday': '921212',
-            'gender': '남자',
-            'job': '직장인'
-          }]
-    );
+  connection.query(
+    'select * from customer',
+    (err,rows,fields) => {
+      res.send(rows);
+    }
+  );  
+    
 });
 
 app.listen(port,() => console.log(`Listening on port of ${port}`));
